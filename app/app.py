ce2767b098bb216e416cbee57198d83598d919a3
Gustavo Lapa <gustavolapa@google.com>
2023-05-25 19:00:04 -0300

    v0.00

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World 0.0!!! (up %s)\n" % elapsed()
+    return "Hello World 0.00!!! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
